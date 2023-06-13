ad1e3ebc9a0f147308eb867c4a108a8f70f2388d
trieuetam2 <trieuetam@gmail.com>
2023-06-13 08:59:27 +0700

    upload asp core mvc

---
+++ Models/LiveStream.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace qlthucung.Models
+{
+    [Table("LiveStream")]
+    public partial class LiveStream
+    {
+        [Key]
+        [Column("idLiveStream")]
+        public int IdLiveStream { get; set; }
+        [Column("noidunglive")]
+        [StringLength(255)]
+        public string Noidunglive { get; set; }
+        [Column("hinh")]
+        [StringLength(255)]
+        public string Hinh { get; set; }
+        [Column("link")]
+        [StringLength(255)]
+        public string Link { get; set; }
+    }
+}
---
+++ Models/petstoreContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace qlthucung.Models
+{
+    public partial class petstoreContext : DbContext
+    {
+        public petstoreContext()
+        {
+        }
+
+        public petstoreContext(DbContextOptions<petstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectsV13;Database=petstore;Trusted_Connection=True;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<AspNetUser>(entity =>
+            {
+                entity.HasIndex(e => e.NormalizedEmail, "EmailIndex");
+
+                entity.HasIndex(e => e.NormalizedUserName, "UserNameIndex")
+                    .IsUnique()
+                    .HasFilter("([NormalizedUserName] IS NOT NULL)");
+
+                entity.Property(e => e.Email).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+
+                entity.Property(e => e.UserName).HasMaxLength(256);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
